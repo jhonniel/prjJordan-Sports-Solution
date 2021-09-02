d0d04422e3b0c2bf6704ba3d4043d224f7b56810
unknown <jhonnielygay99@gmail.com>
2021-09-02 21:22:11 +0800

    Jordan

---
+++ prjJordanSports/frmMain.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAnnual = New System.Windows.Forms.TextBox()
+        Me.lblWeekly = New System.Windows.Forms.TextBox()
+        Me.lblBiweekly = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.BtnPrint = New System.Windows.Forms.Button()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(69, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Annual salary:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(201, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Weekly pay:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(327, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Biweekly pay:"
+        '
+        'txtAnnual
+        '
+        Me.txtAnnual.Location = New System.Drawing.Point(56, 74)
+        Me.txtAnnual.Name = "txtAnnual"
+        Me.txtAnnual.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnnual.TabIndex = 3
+        '
+        'lblWeekly
+        '
+        Me.lblWeekly.BackColor = System.Drawing.SystemColors.Control
+        Me.lblWeekly.Location = New System.Drawing.Point(183, 74)
+        Me.lblWeekly.Name = "lblWeekly"
+        Me.lblWeekly.ReadOnly = True
+        Me.lblWeekly.Size = New System.Drawing.Size(100, 20)
+        Me.lblWeekly.TabIndex = 4
+        '
+        'lblBiweekly
+        '
+        Me.lblBiweekly.Location = New System.Drawing.Point(311, 74)
+        Me.lblBiweekly.Name = "lblBiweekly"
+        Me.lblBiweekly.ReadOnly = True
+        Me.lblBiweekly.Size = New System.Drawing.Size(100, 20)
+        Me.lblBiweekly.TabIndex = 5
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(56, 145)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Location = New System.Drawing.Point(147, 145)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.BtnPrint.TabIndex = 7
+        Me.BtnPrint.Text = "Print"
+        Me.BtnPrint.UseVisualStyleBackColor = True
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(242, 145)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(75, 23)
+        Me.BtnClear.TabIndex = 8
+        Me.BtnClear.Text = "Clear"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(336, 145)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 201)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.BtnPrint)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblBiweekly)
+        Me.Controls.Add(Me.lblWeekly)
+        Me.Controls.Add(Me.txtAnnual)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "                                              Jordan Sports Store"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtAnnual As TextBox
+    Friend WithEvents lblWeekly As TextBox
+    Friend WithEvents lblBiweekly As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents BtnPrint As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ prjJordanSports/frmMain.vb
@@ -0,0 +1,46 @@
+﻿Public Class frmMain
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        System.Windows.Forms.SendKeys.Send("{PRTSC}")
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        txtAnnual.Clear()
+        lblWeekly.Clear()
+        lblBiweekly.Clear()
+        txtAnnual.Focus()
+
+
+
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("en-US")
+        Dim Annual, Weekly, Biweekly As Double
+
+
+        If IsNumeric(txtAnnual.Text) Then
+            Annual = Double.Parse(txtAnnual.Text)
+            Weekly = Annual / 52
+            Biweekly = Annual / 26
+
+            lblWeekly.Text = FormatCurrency(Weekly)
+            lblBiweekly.Text = FormatCurrency(Biweekly)
+        Else
+            lblWeekly.Text = FormatCurrency(0)
+            lblBiweekly.Text = FormatCurrency(0)
+
+
+
+        End If
+    End Sub
+End Class
